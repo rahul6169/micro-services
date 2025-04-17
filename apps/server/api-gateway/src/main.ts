c6b5bc01afb80bc83223bad9f7384ffbcc10aee8
rahul6169 <rahulbalakrishnan6169@gmail.com>
2025-04-17 15:45:42 +0530

    feat: add helmet middleware for security and update package dependencies

--- apps/server/api-gateway/src/main.ts
+++ apps/server/api-gateway/src/main.ts
@@ -6,11 +6,13 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-
+import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.use(helmet());
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
